8080fe6ff2200a63e018ae6e181968a75fe65d1b
JWSun123 <91690088+JWSun123@users.noreply.github.com>
2022-04-20 18:07:29 -0400

    exercise 1

--- src/Array.java
+++ src/Array.java
@@ -11,23 +11,30 @@ public class Array {
         persons[0] = new Person("Sun", "123123123");
 
         for (int i = 0; i < persons.length; i++) {
-            System.out.println(persons[i]);
+//            System.out.println(persons[i]);
         }
         //another way to define an array.
         int[] array = new int[]{1, 2, 3, 4, 5};
 
+// class exercise 1:
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the size of the array:");
 
         try {
             int size = input.nextInt();
             if (size > 0) {
+                //generate an array with the size of the user input.
                 int[] userArray = new int[size];
                 System.out.println("The prime numbers in the array:");
+                boolean isFirst = true;
                 for (int i = 0; i < userArray.length; i++) {
-                    userArray[i] = (int) (Math.random() * 99 + 1);
+                    //generate random numbers between 1 and 100 for the array.
+                    userArray[i] = (int) (Math.random() * 100 + 1);
+                    //print out only the prime numbers.
                     if (isPrime(userArray[i])) {
-                        System.out.print(userArray[i] + ", ");
+                        //print in format using "printf". if it is the first one don't put ", ".
+                        System.out.printf("%s%d", isFirst ? "":", ", userArray[i]);
+                        isFirst = false;
                     }
                 }
             } else {
@@ -44,6 +51,7 @@ public class Array {
         if (n <= 1) {
             return false;
         }
+        //Math.sqrt() returns the square root of a value.
         for (int i = 2; i < Math.sqrt(n); i++) {
             if (n % i == 0) {
                 return false;
